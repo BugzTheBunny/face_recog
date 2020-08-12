b4d4ca5fd76b1d3d3bbfa6e9709740dd83c7cd64
Slava <urigami2010@gmail.com>
2020-08-12 22:53:19 +0300

    Some tweaking, and a readme

--- face_rec.py
+++ face_rec.py
@@ -1,32 +1,40 @@
 import face_recognition
 import os
-import cv2
 
+IMAGE_2_TEST = 'image'
+
+# Folders
 KNOWN_DIR = 'known'
 UNKNOWN_DIR = 'unknown'
-# GROUP_DIR = 'groups'
-MODEL = 'cnn'  # hog - Less accurate, but faster , cnn - Slow, but more accurate
-TOLERANCE = 0.6  # For testing the tolerance - default 0.6
+MODEL = 'cnn'  # 'hog' - Less accurate, but much faster , 'cnn' - Slower but more accurate.
+TOLERANCE = 0.6  # For testing the tolerance - default 0.6 (higher -> less accurate)
 
-unknown_face = f'{UNKNOWN_DIR}/image.jpg'
+unknown_face = f'{UNKNOWN_DIR}/{IMAGE_2_TEST}.jpg'
 
 
 def main():
+    # "database"
     known_faces = []
     known_faces_encoded = []
 
     for image in os.listdir(KNOWN_DIR):
+        # loads the image.
         image_load = face_recognition.load_image_file(f'{KNOWN_DIR}/{image}')
-        image_load = cv2.cvtColor(image_load, cv2.COLOR_RGB2BGR)
-        image_encoding = face_recognition.face_encodings(image_load)[0]
+        # encodes the image to the face_recognition format - array for each face.
+        image_encoding = face_recognition.face_encodings(image_load)[0]  # Selecting the first array
+        # adding the encoding version into an array.
         known_faces.append(image_encoding)
+        # adding into a "temp db"
         known_faces_encoded.append({'name': image.replace('.jpg', ''), 'encoded': image_encoding})
 
     try:
+        # Loading the image that should be compared - in a try catch, the library expects to find a face.
         unknown_face_image = face_recognition.load_image_file(unknown_face)
         unknown_face_encoding = face_recognition.face_encodings(unknown_face_image)[0]
+        # So this line does all the magic, it tries to compare each face from the db to the unknown face
         results = face_recognition.compare_faces(known_faces, unknown_face_encoding, tolerance=TOLERANCE)
         if True in results:
+            # Just a pretty print with some data.
             print(f'Found {known_faces_encoded[results.index(True)]["name"]}\nTF = {results}')
         else:
             print('Did not match..')
---
+++ readme.md
@@ -0,0 +1,16 @@
+# Basic face recognition project
+
+`python3.7` `face_recognition`
+
+**face_recognition Docs** - https://face-recognition.readthedocs.io/en/latest/
+
+
+This script should recognize faces.
+
+The `known` folder, contains all of the faces that may be recognized, in case you want to add another one, just save the
+image over there, with the person name. 
+
+The `unknown` folder, should contain the image of the person you want to try to recognize, the image should be saved as image.jpg (You can of course change it in the code, its pretty modular)
+
+In the near future I will add the option to make it learn to register new faces.
+
